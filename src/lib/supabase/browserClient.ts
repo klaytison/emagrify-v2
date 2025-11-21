0a67f07decb7a3188b7c8809f2376db7090e3999
klaytison <klaytsa3@gmail.com>
2025-11-21 18:23:35 -0300

    Create browserClient.ts

---
+++ src/lib/supabase/browserClient.ts
@@ -0,0 +1,8 @@
+// src/lib/supabase/browserClient.ts
+import { createBrowserClient } from "@supabase/ssr";
+
+export const createBrowserSupabaseClient = () =>
+  createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  );
