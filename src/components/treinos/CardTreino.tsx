189e533ce885b2f0b97d500d0dba6d0ddfa12702
klaytison <klaytsa3@gmail.com>
2025-11-29 18:19:10 -0300

    Update CardTreino.tsx

--- src/components/treinos/CardTreino.tsx
+++ src/components/treinos/CardTreino.tsx
@@ -1,1 +1,9 @@
+"use client";
 
+export default function CardTreino({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="bg-gray-900/50 border border-gray-800 px-6 py-8 rounded-2xl w-full max-w-xl mx-auto flex flex-col gap-6">
+      {children}
+    </div>
+  );
+}
